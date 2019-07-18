4f1af54174156ddee3edb44f2a74541ca04a11c7
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-07-18 20:06:44 +0700

    tambah card

---
+++ client/src/Card.vue
@@ -0,0 +1,46 @@
+<template>
+    <div style="padding-top: 20px;">
+        <div class="ui card" style="width: 100%">
+            <div class="content">
+                <div class="header">{{list.userId.username}}</div>
+                <div class="description">
+                    <p>{{list.link}}</p>
+                </div>
+            </div>
+            <div class="extra content">
+                <span class="left floated like">
+                   <a href="#"><i class="like icon"></i></a> 
+                </span>
+                <span class="center floated star">
+                    <a href="#" @click.prevent="addfav(list.id)"><i class="star icon"></i></a>
+                </span>
+                <span class="right floated">
+                    <a href="http://twitter.com/share">Share with Twitter</a>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props:['list'],
+    methods:{
+        addFav(id){
+            axios({
+                url:`http://localhost/audios/${id}`,
+                method: "PUT",
+                data: {
+                    
+                }
+            })
+            .then
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
