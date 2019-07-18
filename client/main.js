32200032a5b35f02b52e61bed15666e4c2c92563
Rhamadan Yahya <Rhamdan25@gmail.com>
2019-07-18 23:42:08 +0700

    navbar sidebar

---
+++ client/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './src/App.vue';
+import AudioRecorder from "vue-audio-recorder"
+
+Vue.use(AudioRecorder)
+
+new Vue(App).$mount('#app');
---
+++ client/src/sidebar.vue
@@ -0,0 +1,45 @@
+<template>
+<div style="margin-top :50px; position: fixed" >
+
+<div class="ui vertical menu">
+  <div class="item">
+    <div class="ui input"><input type="text" placeholder="Search..."></div>
+  </div>
+  <div class="item">
+    <a class = "item">Home</a>
+    <div class="menu">
+      <a @click="getAllAudio" class="item">Your Audio</a>
+      <a class="item">Favourites</a>
+    </div>
+  </div>
+  <a @click="getUserAudio" class="item">
+    <i class="grid layout icon"></i> Browse
+  </a>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default{
+    name:"sidebar",
+    data() {
+        return {        
+        };
+    },
+    components:{
+    },
+    methods:{
+        getAllAudio(){
+
+        },
+        getUserAudio(){
+
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+</style>
