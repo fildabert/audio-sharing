d445ca9f3a23704a8441eaee69cad640307d2103
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-18 21:46:43 +0700

    aneh ada yang masih konflik tapi bisa di push?

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,8 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
 
-<<<<<<< HEAD
 router.post('/signup', userController.signup)
 router.post('/signin',userController.signin)
-=======
-router.post('/', userController.signin)
-router.post('/',userController.signup)
->>>>>>> 3281874acc842331bacd6736832b63a17f2ef003
 
 
 module.exports = router
