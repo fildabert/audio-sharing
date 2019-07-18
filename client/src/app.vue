6e9b1f4e01fbbd96df9a9dfcd80f045a6ceadd9d
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-18 21:42:59 +0700

    udah

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,14 +1,32 @@
-<template>
+<template lang="html">
+<div>
   <h1>{{ message }}</h1>
+  <signin @triggerSignUp="triggerSignUp" v-show="showSignIn"></signin>
+  <register v-show="showSignUp"></register>
+</div>
 </template>
 
 <script>
+import signin from "./signin";
+import register from "./register"
 export default {
+  components : {
+    signin,
+    register
+  },
   data() {
     return {
       message: 'Hello world',
+      showSignUp: false,
+      showSignIn : true,
     };
   },
+  methods: {
+    triggerSignUp: function() {
+      this.showSignUp = true
+      this.showSignIn = false
+    }
+  }
 };
 </script>
 
