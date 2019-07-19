b60446504675bc8c6cc5c9d4f0086820f9ae236b
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-19 09:34:56 +0700

    merge

--- client/src/app.vue
+++ client/src/app.vue
@@ -1,55 +1,32 @@
-<template lang="html">
-<div>
-  <Home v-if="showHome"></Home>
-  <signin @triggerSignUp="triggerSignUp" @triggerHomePage="toggleHomePage" v-show="showSignIn"></signin>
-  <register @triggerSignIn="toggleSignIn" @triggerHome="toggleHomePage" v-show="showSignUp"></register>
-</div>
+<template>
+    <div>
+          <Home></Home>
+        <!-- <button @click="record">Record</button>
+        <button id="stoprecord">Stop Recording</button> -->
+        
+    </div>
 </template>
 
 <script>
-import signin from "./signin";
-import register from "./register"
-import Home from "./Home"
+import axios from "axios"
+import Home from './Home.vue'
+
+
 export default {
-  components : {
-    signin,
-    register,
-    Home
-  },
-  created() {
-    this.toggleHomePage()
-  },
-  data() {
-    return {
-      message: 'Hello world',
-      showSignUp: false,
-      showSignIn : true,
-      showHome: false
-    };
-  },
-  methods: {
-    triggerSignUp: function() {
-      this.showSignUp = true
-      this.showSignIn = false
+     components:{
+        'Home' : Home
     },
-    toggleHomePage : function () {
-      const token = localStorage.getItem("token")
-      if(token){
-        this.showHome = true
-        this.showSignUp = false
-        this.showSignIn = false
-      } else {
-        this.showHome = false
-        this.showSignIn = true
-      }
+    data () {
+        return {
+            audioLink: ""
+        }
     },
-    toggleSignIn : function () {
-      this.showSignIn = true
-      this.showSignUp = false
+    methods: {
+        
     }
-  }
-};
+}
 </script>
 
-<style scoped>
+<style>
+
 </style>
